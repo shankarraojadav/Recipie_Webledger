ae52eece8f3a4bc0e9b5a24d0fa510a3b2f78153
SHANKAR JADAV <shankarjadav360@gmail.com>
2023-11-07 20:52:36 +0530

    changes

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const url = "http://localhost:4000";
+const url = "http://localhost:4000" || "https://recipe-6w2j.onrender.com";
 
 export const googleAuth = createAsyncThunk(
   "signin",
